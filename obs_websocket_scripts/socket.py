970c2313888110e0a796b1760e548da801f57a5d
Allison Yao <allison98@gmail.com>
2021-01-09 21:27:47 -0800

    class python app, switches between 2 vids on time

--- obs_websocket_scripts/socket.py
+++ obs_websocket_scripts/socket.py
@@ -1,23 +1,3 @@
-
-# import simpleobsws
-# import asyncio
-
-
-# loop = asyncio.get_event_loop()
-# ws = simpleobsws.obsws(host='localhost', port=4444, password='nwhacks', loop=loop) # Every possible argument has been passed, but none are required. See lib code for defaults.
-
-# async def make_request():
-#     await ws.connect() # Make the connection to OBS-Websocket
-#     result = await ws.call('GetVersion') # We get the current OBS version. More request data is not required
-#     print(result) # Print the raw json output of the GetVersion request
-#     await asyncio.sleep(1)
-#     data = {'source':'test_source', 'volume':0.5}
-#     result = await ws.call('SetVolume', data) # Make a request with the given data
-#     print(result)
-#     await ws.disconnect() # Clean things up by disconnecting. Only really required in a few specific situations, but good practice if you are done making requests or listening to events.
-
-# loop.run_until_complete(make_request())
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -28,27 +8,84 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 sys.path.append('../')
-from obswebsocket import obsws, requests  # noqa: E402
+from obswebsocket import obsws, requests, events  # noqa: E402
+
+class socket:
+
+    def __init__(self):
+        self.start = time.time()
+        self.end = 0
+        self.timer_done = 1
+        self.neutral = 0
+        self.happy = 0
+
+    def on_switch(self, message):
+        print(u"You changed the scene to {}".format(message.getSceneName()))
 
 
-host = "localhost"
-port = 4444
-password = "nwhacks"
+    def on_end(self,message):
+        print(u"This scene ended {}".format(message.getName()))
+        
 
-ws = obsws(host, port, password)
-ws.connect()
+    def vid_timer(self):
+        self.end = time.time()
+        elapsed = self.end - self.start
+        if elapsed > 15:
+            self.timer_done = 1
+            self.start = time.time()
+            self.neutral = not self.neutral
+            self.happy = not self.happy
 
-try:
-    scenes = ws.call(requests.GetSceneList())
-    for s in scenes.getScenes():
-        name = s['name']
-        print(u"Switching to {}".format(name))
-        ws.call(requests.SetCurrentScene(name))
-        time.sleep(2)
+    def check_for_laughs(self):
+        pass
 
-    print("End of list")
+    def check_for_negatives(self):
+        pass
 
-except KeyboardInterrupt:
-    pass
+    def check_for_name(self):
+        pass
 
-ws.disconnect()
+    def run(self):
+        # connect to OBS
+        host = "localhost"
+        port = 4444
+        password = "nwhacks"
+
+        ws = obsws(host, port, password)
+        ws.register(self.on_switch, events.SwitchScenes)
+        ws.register(self.on_end, events.TransitionVideoEnd)
+        ws.connect()
+
+        self.neutral = 1
+        self.happy = 0
+        negative = 0
+
+        while(1):
+
+            self.check_for_laughs()
+            self.check_for_negatives()
+            self.check_for_name()
+
+            # timer at 15 seconds to wait for video to finish playing
+            # need to have check laugh and check neg bypass that later
+            try:
+                if (self.neutral == 1) & (self.timer_done == 1):
+                    ws.call(requests.SetCurrentScene("Neutral"))
+                    self.timer_done = 0
+                elif (self.happy == 1) & (self.timer_done == 1):
+                    ws.call(requests.SetCurrentScene("Happy"))
+                    self.timer_done = 0
+                elif (negative == 1) & (self.timer_done == 1):
+                    ws.call(requests.SetCurrentScene("Negative"))
+                    self.timer_done = 0
+
+                self.vid_timer()
+                
+            except KeyboardInterrupt:
+                break
+
+        ws.disconnect()
+
+
+app = socket()
+app.run()
